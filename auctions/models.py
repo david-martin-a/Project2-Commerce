614f67fc84946fae59b42e6b985bc748a9371f62
David <david.alwin@gmail.com>
2025-03-12 14:54:30 -0400

    Allow number on watchlist to be blank

--- auctions/models.py
+++ auctions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    number_on_watchlist = models.CharField(max_length=8, default="")
+    number_on_watchlist = models.CharField(blank=True, max_length=8)
 
 class Categories(models.Model):
     category = models.CharField(max_length=64)
